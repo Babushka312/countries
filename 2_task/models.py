6ada4f60b860b34ce05740ac77cb125626c42544
Your Name <you@example.com>
2022-08-15 15:08:08 +0600

    first commit

---
+++ 2_task/models.py
@@ -0,0 +1,28 @@
+from peewee import *
+
+db = PostgresqlDatabase(
+    'countries',
+    host = 'localhost',
+    port = '5432',
+    user = 'superman',
+    password = 'qwe123'
+)
+
+db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class apishka(BaseModel):
+    name = CharField(max_length=255, null=False)
+    official_name = CharField(max_length=255, null=False)
+    capital = CharField(max_length=255, null=False)
+    region = CharField(max_length=255, null=False)
+    subregion = CharField(max_length=255, null=False)
+    population = IntegerField()
+    continents = CharField(max_length=255, null=False)
+    timezones = CharField(max_length=255, null=False)
+
+db.create_tables([apishka])    
+db.close()
